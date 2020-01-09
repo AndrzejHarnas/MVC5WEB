860d372282c3dc88b58f57ddbde9ac7b6c56ee60
Wieslaw Szott <szott@KZ-L-SZOTT-01.inig.local>
2020-01-09 16:29:59 +0100

    Dodaj pliki projektów.

---
+++ MVC5WEB/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "To jest stronka w MVC5!!!!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.TheMessage = "Masz jakiś problem.Napisz do nas!";
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            ViewBag.TheMessage = "Dzięki mamy twoją wiadomość!!";
+
+            return View();
+        }
+
+        public ActionResult Foo()
+        {
+            return View("About");
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVC%ATM1";
+
+            if (letterCase == "lower") {
+                return Content(serial.ToLower());
+            }
+            else{
+                // return Content(serial);
+                return Json(new { name = "serial", value = serial },JsonRequestBehavior.AllowGet);
+
+            }
+        }
+    }
+}
